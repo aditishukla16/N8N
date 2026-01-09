15e98b9d99479f9e8dce21ae1a60f40481dd06db
Aditi Shukla <aditishukla1676@gmail.com>
2026-01-09 20:30:18 +0530

    feat(workflow): add workflow controller

---
+++ apps/backend/src/controller/workflow.controller.ts
@@ -0,0 +1,77 @@
+import { Request, Response } from "express";
+import { prisma } from "../lib/prisma";
+
+
+//CREATE WORKFLOW
+export const createWorkflow = async (req: Request, res: Response) => {
+    try {
+        const userId = req.user.id;
+        const { title, nodes, connections } = req.body;
+
+        if (!title || !nodes || !connections) {
+            return res.status(400).json({
+                error: "Title, nodes and connections are required",
+            });
+        }
+        const workflow = await prisma.workflow.create({
+            data: {
+                title,
+                nodes,
+                connections,
+                userId,
+                enabled: true,
+            },
+        });
+        return res.status(201).json({
+            message: "Workflow created",
+            workflow,
+
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+//GET ALL WORKFLOWS (for logged-in user)
+export const getWorkflows = async (req: Request, res: Response) => {
+    try {
+        const userId = req.user.id;
+        const workflows = await prisma.workflow.findMany({
+            where: { userId },
+            select: {
+                id: true,
+                title: true,
+                enabled: true,
+                createdAt: true,
+            },
+        });
+        return res.json({ workflows });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+};
+//GET WORKFLOW BY ID
+export const getWorkflowById = async (req: Request, res: Response) => {
+    try {
+        const userId = req.user.id;
+        const workflowId = req.params.id;
+        const workflow = await prisma.workflow.findFirst({
+            where: {
+                id: workflowId,
+                userId,
+            },
+        });
+        if (!workflow) {
+            return res.status(404).json({ error: "Workflow not found" });
+        }
+
+        return res.json({ workflow });
+
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
